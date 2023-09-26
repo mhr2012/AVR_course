232d80777a8d847e6b4cd3eb84364585f79efcf6
mhr2012 <mhr2012mhr2013@gmail.com>
2023-09-26 06:12:24 +0330

    interrupt INT0

---
+++ P14_Interrupt/main.c
@@ -0,0 +1,20 @@
+ 
+ #include <mega32a.h>
+interrupt [2] void ext_int0_isr(void)
+{
+ 
+ PORTA.0 = !PORTA.0;
+}
+void main(void){
+
+DDRA.0 = 1;
+
+
+GICR = (0<<INT1) | (1<<INT0) | (0<<INT2);
+
+MCUCR = (0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+#asm("sei")
+while (1){
+
+}
+}
